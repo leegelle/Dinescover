f070fb19e5355f9d261cda3511b79623841aeec0
lgelle3 <lgelle3@gmail.com>
2026-02-03 01:46:18 -0600

    iOS app

---
+++ RestrauntPickerTests/RestrauntPickerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestrauntPickerTests.swift
+//  RestrauntPickerTests
+//
+//  Created by Liiban on 12/2/25.
+//
+
+import Testing
+@testable import RestrauntPicker
+
+struct RestrauntPickerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
